ef270b82b4e66fea2c391e103c7421fb0d4ee802
GAEL PEREIRA <gael.pereira@utp.ac.pa>
2025-04-06 23:55:27 -0500

    ✨ 🐛 nuevos recursos y ajustes al contenido

--- Scripts.js
+++ Scripts.js
@@ -1,3 +1,24 @@
+/*document.querySelectorAll(".carrusel").forEach((carrusel) => {
+    carrusel.addEventListener("mouseover", () => {
+        const tools = carrusel.querySelectorAll("a"); // Solo selecciona elementos dentro de este carrusel
+        tools.forEach((tool) => {
+            tool.style.animationPlayState = "paused";  // Pausar solo los elementos de este carrusel
+        });
+    });
+
+    carrusel.addEventListener("mouseleave", () => {
+        const tools = carrusel.querySelectorAll("a");
+        tools.forEach((tool) => {
+            tool.style.animationPlayState = "running"; // Reanudar solo los elementos de este carrusel
+        });
+    });
+}); */
+
+const btnMenu = document.querySelector('.btnMenu');
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('active');
+});
+
 document.querySelectorAll(".carrusel").forEach((carrusel) => {
     carrusel.addEventListener("mouseover", () => {
         const tools = carrusel.querySelectorAll("a"); // Solo selecciona elementos dentro de este carrusel
